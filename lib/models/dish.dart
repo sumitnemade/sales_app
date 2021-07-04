de0325465912087200909da6d6e8d0b1583e39e8
sumit nemade <nemadesumit@gmail.com>
2021-07-04 09:17:38 +0530

    get data logic optimized

--- lib/models/dish.dart
+++ lib/models/dish.dart
@@ -14,26 +14,4 @@ class Dish {
     this.minQuantity,
     this.maxQuantity,
   });
-
-  factory Dish.fromJson(Map<String?, dynamic> json) {
-    return Dish(
-      sku: json['sku'],
-      totalPrice: json['totalPrice'],
-      quantity: json['quantity'],
-      maxQuantity: json['maxQuantity'],
-      totalOrder: json['totalOrder'],
-      minQuantity: json['minQuantity'],
-    );
-  }
-
-  Map<String?, dynamic> toJson() {
-    final Map<String?, dynamic> data = new Map<String?, dynamic>();
-    data['sku'] = this.sku;
-    data['totalPrice'] = this.totalPrice;
-    data['quantity'] = this.quantity;
-    data['minQuantity'] = this.minQuantity;
-    data['maxQuantity'] = this.maxQuantity;
-    data['totalOrder'] = this.totalOrder;
-    return data;
-  }
 }
--- lib/models/monthly_sale.dart
+++ lib/models/monthly_sale.dart
@@ -22,32 +22,4 @@ class MonthlySales {
     this.minQuantity,
     this.popularRevenueItem,
   });
-
-  factory MonthlySales.fromJson(Map<String?, dynamic> json) {
-    return MonthlySales(
-      totalQuantity: json['totalQuantity'],
-      popularRevenueItem: json['popularRevenueItem'],
-      popularItem: json['popularItem'],
-      maxQuantity: json['maxQuantity'],
-      totalPrice: json['totalPrice'],
-      popularItemAvg: json['popularItemAvg'],
-      minQuantity: json['minQuantity'],
-      allEntries: json['allEntries'],
-    );
-  }
-
-  Map<String?, dynamic> toJson() {
-    final Map<String?, dynamic> data = new Map<String?, dynamic>();
-
-    data['totalQuantity'] = this.totalQuantity;
-    data['popularRevenueItem'] = this.popularRevenueItem;
-    data['popularItem'] = this.popularItem;
-    data['maxQuantity'] = this.maxQuantity;
-    data['totalPrice'] = this.totalPrice;
-    data['popularItemAvg'] = this.popularItemAvg;
-    data['minQuantity'] = this.minQuantity;
-    data['allEntries'] = this.allEntries;
-
-    return data;
-  }
 }
